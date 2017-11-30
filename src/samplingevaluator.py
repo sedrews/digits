b31a7e11212387680948235ec0dcbda440a911e1
Samuel Drews <sedrews@wisc.edu>
2017-11-29 22:58:54 -0600

    sample postconditions

--- src/samplingevaluator.py
+++ src/samplingevaluator.py
@@ -9,8 +9,15 @@ class SamplingEvaluator(Evaluator):
         self.num_samples = num_samples
 
     def compute_post(self, prog):
-        # need a way to evaluate postcondition expressions in general
-        pass
+        samples = [self.sampler.next_sample() for j in range(self.num_samples)]
+        samples = [(*sample, prog(*sample)) for sample in samples] # Include the output value
+        samples = [dict(zip(range(len(sample)), sample)) for sample in samples] # XXX requires the same indexing for the variable values as in the definitions of the events in post
+        counter = {event : 0 for event in self.post.probs}
+        for sample in samples:
+            for event,value in post.evaluate_prob_events(sample).items():
+                counter[event] += 1 if value else 0
+        estimates = {event : counter[event] / len(samples) for event in counter}
+        return post.evaluate_expression(estimates)
 
     def compute_error(self, prog):
         pass
